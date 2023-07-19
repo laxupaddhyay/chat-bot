208be94fa5c0e1c8b1b151e1ad3ee3105365727e
LAKSHYA UPADDHYAY <95523785+laxupaddhyay@users.noreply.github.com>
2023-07-19 17:51:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const chatbox = document.getElementById('chatbox');
+const input = document.getElementById('input');
+const submit = document.getElementById('submit');
+
+// Function to add a new message to the chatbox
+function addMessage(message, sender) {
+    const msg = document.createElement('div');
+    msg.innerHTML = `<strong>${sender}: </strong>${message}`;
+    chatbox.appendChild(msg);
+    chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+// Function to send a message to the server
+function sendMessage() {
+    const message = input.value;
+    if (message.trim() === '') return;
+
+    addMessage(message, 'You');
+    input.value = '';
+
+    fetch('http://13.233.72.40:5000/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ message: message })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const reply = data.reply;
+        addMessage(reply, 'Bot');
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+}
+
+// Event listener for the submit button
+submit.addEventListener('click', sendMessage);
+
+// Event listener for the Enter key
+input.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        sendMessage();
+    }
+});
